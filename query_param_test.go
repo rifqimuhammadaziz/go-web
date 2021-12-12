879fcb5c05b819e6c812270c8670ac413ef153d7
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2021-12-12 18:41:21 +0700

    add TestQueryParameter

---
+++ query_param_test.go
@@ -0,0 +1,31 @@
+package goweb
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func SayHello(writer http.ResponseWriter, request *http.Request) {
+	// domainName/?name=Rifqi
+	name := request.URL.Query().Get("name")
+	if name == "" {
+		fmt.Fprint(writer, "Hello")
+	} else {
+		fmt.Fprintf(writer, "Hello %s", name)
+	}
+}
+
+func TestQueryParameter(t *testing.T) {
+	request := httptest.NewRequest("GET", "http://localhost:8080/?name=Xenosty", nil)
+	recorder := httptest.NewRecorder()
+
+	SayHello(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
