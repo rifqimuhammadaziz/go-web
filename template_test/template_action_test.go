c9e828fb92154eee17d875a0152bacc13d495fcc
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2021-12-15 09:42:18 +0700

    add TestTemplateActionRange for output array in gohtml

--- template_test/template_action_test.go
+++ template_test/template_action_test.go
@@ -48,3 +48,23 @@ func TestTemplateActionOperator(t *testing.T) {
 	body, _ := io.ReadAll(recorder.Result().Body)
 	fmt.Println(string(body))
 }
+
+func TemplateActionRange(rw http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("../templates/range.gohtml"))
+	t.ExecuteTemplate(rw, "range.gohtml", map[string]interface{}{
+		"Title": "Test Template Action Range",
+		"Hobbies": []string{
+			"Play Dota2", "Learning Go & Cloud", "Watching Serial",
+		},
+	})
+}
+
+func TestTemplateActionRange(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateActionRange(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
