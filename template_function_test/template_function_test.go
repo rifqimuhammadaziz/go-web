24e20f4d24a7f10013f49c99e801931800e7c7c3
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2021-12-15 18:39:32 +0700

    add TestTemplateFunctionGlobalCreate for create function upper string in gohtml

---
+++ template_function_test/template_function_test.go
@@ -0,0 +1,82 @@
+package goweb
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+type MyPage struct {
+	Name string
+}
+
+func (myPage MyPage) SayHello(name string) string {
+	return "Hello " + name + ", My Name Is " + myPage.Name
+}
+
+func TemplateFunction(rw http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.New("FUNCTION").Parse(`{{.SayHello "Xenosty"}}`))
+	t.ExecuteTemplate(rw, "FUNCTION", MyPage{
+		Name: "Xenosty",
+	})
+}
+
+func TestTemplateLayout(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateFunction(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TemplateFunctionGlobal(rw http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.New("FUNCTION").Parse(`{{len "Xenosty"}}`))
+	t.ExecuteTemplate(rw, "FUNCTION", MyPage{
+		Name: "Test Template Function Global",
+	})
+}
+
+func TestTemplateFunctionGlobal(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateFunctionGlobal(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TemplateFunctionGlobalCreate(rw http.ResponseWriter, r *http.Request) {
+	t := template.New("GlobalFunction")
+
+	// create template function
+	t = t.Funcs(map[string]interface{}{
+		// functionName: createFunction | upper(value string)
+		"upper": func(value string) string {
+			return strings.ToUpper(value)
+		},
+	})
+
+	// execute template function upper(Name)
+	t = template.Must(t.Parse(`{{ upper .Name }}`))
+
+	t.ExecuteTemplate(rw, "GlobalFunction", MyPage{
+		Name: "Test Template Function Global",
+	})
+}
+
+func TestTemplateFunctionGlobalCreate(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateFunctionGlobalCreate(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
