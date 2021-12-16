46c0be4454b83ac84587bb1bf065e2ae1d2995dc
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2021-12-16 09:34:46 +0700

    add test to disable auto escape and test xss

--- template_auto_escape_test/post_test.go
+++ template_auto_escape_test/post_test.go
@@ -44,3 +44,61 @@ func TestTemplateAutoEscapeServer(t *testing.T) {
 		panic(err)
 	}
 }
+
+func TemplateAutoEscapeDisabled(rw http.ResponseWriter, r *http.Request) {
+	myTemplates.ExecuteTemplate(rw, "post.gohtml", map[string]interface{}{
+		"Title": "Test Template Auto Escape",
+		"Body":  template.HTML("<p>This is Body</p>"), // rendered in html, auto escape disabled, but potential to xss (cross site scripting)
+	})
+}
+
+func TestTemplateAutoEscapeDisabled(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateAutoEscapeDisabled(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TestTemplateAutoEscapeDisabledServer(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(TemplateAutoEscapeDisabled),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TemplateXSS(rw http.ResponseWriter, r *http.Request) {
+	myTemplates.ExecuteTemplate(rw, "post.gohtml", map[string]interface{}{
+		"Title": "Test Template Auto Escape",
+		"Body":  template.HTML(r.URL.Query().Get("body")), // rendered in html, auto escape disabled, but potential to xss (cross site scripting)
+	})
+}
+
+func TestTemplateXSS(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/?body=<p>Hacked</p>", nil) // XSS, tag html rendered
+	recorder := httptest.NewRecorder()
+
+	TemplateXSS(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TestTemplateXSSServer(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(TemplateXSS),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
